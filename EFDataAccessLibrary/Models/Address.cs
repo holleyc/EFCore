c22c182c4393094f39aaca48328e378941841da8
Christopher Holley <chris57575@hotmail.com>
2020-01-20 20:19:45 -0500

    Add project files.

---
+++ EFDataAccessLibrary/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string StreetAddress { get; set; }
+
+
+
+    }
+}
