4c0832c6a14fbc254ea8dd6bc5671decc4b1d636
Christopher Holley <chris57575@hotmail.com>
2020-01-20 20:41:28 -0500

    Adding in the Person class

--- EFDataAccessLibrary/Models/Person.cs
+++ EFDataAccessLibrary/Models/Person.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace EFDataAccessLibrary.Models
 {
@@ -10,6 +8,7 @@ namespace EFDataAccessLibrary.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-        public List<Address> Addresses { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        public List<Email> EmailAddresses { get; set; } = new List<Email>();
     }
 }
