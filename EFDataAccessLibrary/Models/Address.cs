4b6323827262dc53b7ef274175c5e58bb65699eb
Christopher Holley <chris57575@hotmail.com>
2020-01-20 20:29:13 -0500

    Added in the address model.

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -7,10 +7,10 @@ namespace EFDataAccessLibrary.Models
     public class Address
     {
         public int Id { get; set; }
-
         public string StreetAddress { get; set; }
-
-
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
 
     }
 }
