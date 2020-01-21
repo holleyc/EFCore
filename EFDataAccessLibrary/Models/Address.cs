49f7f5bbca25c6c34ae8cc428c305128891555bb
Christopher Holley <chris57575@hotmail.com>
2020-01-20 21:56:32 -0500

    Adding Data Annotations

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFDataAccessLibrary.Models
 {
     public class Address
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string StreetAddress { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string State { get; set; }
+
+        [Required]
+        [MaxLength(10)] // 12312-1241
+        [Column(TypeName = "varchar(10")]
         public string ZipCode { get; set; }
 
     }
